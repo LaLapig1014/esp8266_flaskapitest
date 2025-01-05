bc5ce0ac831280987b7b99a3c69d3a184c8221da
lalapig <a28065872@gmail.com>
2025-01-05 17:59:29 +0800

    firebase set_0105_2

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 from flask import Flask, request, render_template
 from flask import Flask, send_from_directory
 from flask import Flask, jsonify
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
 import json
+
+cred = credentials.Certificate("esp8266-apitest-firebase-adminsdk-iwwxn-eca0c378ea.json")  # 替換為你的密鑰路徑
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://esp8266-apitest-default-rtdb.asia-southeast1.firebasedatabase.app/'  # 替換為你的 Realtime Database URL
+})
+
+
+
+def get_data_from_realtime_db():
+    ref = db.reference('ESP8266')  # 替換為你需要讀取的節點名稱
+    data = ref.get()
+    print(data)
+    return data
+def update_data_in_realtime_db(ID,status):
+    ref = db.reference('ESP8266')  # 替換為你需要更新的節點名稱
+    new_data = {
+        "ID": ID,
+        "status": status
+    }
+    ref.set(new_data)  # 完全覆蓋該節點的資料
+    print("Data updated successfully!")
+
 def getpin():
     pin=""
     for i in range(6):
@@ -12,10 +38,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     msg=""
-    with open('data.json', 'r') as file:
-        datas = json.load(file)
-    M=datas["ESP8266"][0]
-    msg=M["ID"]+","+M["state"]
+    D=get_data_from_realtime_db()
+    msg=D["ID"]+","+D["status"]
     
     print(msg)
     return render_template("index.html",msg=msg)
@@ -26,9 +50,9 @@ def yanwei():
 
 @app.route("/getdata",methods=["GET"])
 def getdata():
-    with open('data.json', 'r') as file:
-        datas = json.load(file)
-    D=datas["ESP8266"][0]
+
+    D=get_data_from_realtime_db()
+    
     return jsonify(D)
 
 @app.route("/putdatajson",methods=["PUT"])
@@ -36,9 +60,9 @@ def putdatajson():
     if request.is_json:
         data = request.get_json() 
         MID=data.get("MID")
-        state = data.get('state')
-        print(MID,state)
-        return jsonify({"message": f"Received data: ID={MID}, state={state}"}), 200
+        status = data.get('status')
+        print(MID,status)
+        return jsonify({"message": f"Received data: ID={MID}, status={status}"}), 200
     else:
         return jsonify({"error": "Request must be JSON"}), 400
     
@@ -47,19 +71,15 @@ def putdataurl():
     params = request.args.to_dict()
     print(params)
     MID=params["MID"]
-    state=params["state"]
+    status=params["status"]
+
+    update_data_in_realtime_db(MID,status)
+
     #--
-    with open('./data.json','r+',encoding="UTF-8") as jsonf:
-            data=json.load(jsonf)
-            for it in data["ESP8266"]:
-                it["ID"]=MID
-                it["state"]=state
-                jsonf.seek(0)  # 將指標移回文件開頭
-                json.dump(data, jsonf, ensure_ascii=False, indent=4) 
-                jsonf.truncate()
+    
     #--
-    return jsonify({"message": f"Received data:MID:{MID},state:{state}"}), 200
-    #http://192.168.38.100:8080/putdataurl?MID=A03&state=HIHI
+    return jsonify({"message": f"Received data:MID:{MID},status:{status}"}), 200
+    #http://192.168.38.100:8080/putdataurl?MID=A03&status=HIHI
 @app.route("/test",methods=["GET"])
 def test():
 
@@ -70,8 +90,8 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
-    #app.run(host="local", port=8080, debug=True)
-    app.run(debug=True)
+    app.run(host="localhost", port=8080, debug=True)
+    #app.run(debug=True)
 
 
 #render_template將會找尋html檔案傳送給使用者
